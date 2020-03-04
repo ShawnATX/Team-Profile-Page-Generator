08dcf73d79d5dcbd308462107ab53589891e67ca
Shawn Wright <volushawn@gmail.com>
2020-03-04 15:29:34 -0600

    HTML file writing finished

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ const checkEmailAddress = (email) => {
     }
 };
 
+const writeTeamPage = (content) => {
+    fs.writeFile(outputPath, content, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success!");
+      });
+}
+
+
 const managerQuestions = [
     {
         type: "input",
@@ -149,6 +159,8 @@ const getUserInput = async () => {
                 default:
                     teamFinished = true;
                     const newTeamPage = render(teamArr);
+                    writeTeamPage(newTeamPage);
+                    console.log("Team page created!");
                     break;
             }
         }
@@ -160,22 +172,9 @@ const getUserInput = async () => {
 
 getUserInput();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
